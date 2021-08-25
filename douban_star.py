84c0cebe8fc8b775952b1667c236dc8b5c59dfe5
lxue <30921318+titxue@users.noreply.github.com>
2021-08-25 14:15:20 +0800

    添加headers 修复因为headers user-agent不能正常运行的问题
    添加headers 修复因为headers user-agent不能正常运行的问题

--- douban_star.py
+++ douban_star.py
@@ -1,6 +1,7 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
+# user-agent
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
 url = "https://movie.douban.com/top250"
 # 设置headers
